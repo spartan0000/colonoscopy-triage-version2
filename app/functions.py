0bead5e695386d040af4874e075a08247193f2cd
David Lee <doctor.dlee@gmail.com>
2025-11-20 00:07:58 +1300

    fix triage function

--- app/functions.py
+++ app/functions.py
@@ -66,13 +66,14 @@ async def format_query_json(user_query: str) -> dict:
 
 
 def triage(data: dict):
+    follow_up = None
     patient_age = data['patient_age']
     indication = data.get('indication', '')
     total_polyps = data['colonoscopy'][0]['number_of_polyps']
     cecum = data['colonoscopy'][0]['cecum_reached']
     bbps = data['colonoscopy'][0]['bostonBowelPrepScore']
     hist = data['colonoscopy'][0]['histology']
-    n_adenoma = hist['adenoma']
+    n_adenoma = hist['adenomas']
     size_adenoma = hist['adenoma_size']
     hgd_adenoma = hist['high_grade_dysplasia_in_adenoma']
     tva = hist['tubulovillous_or_villous_adenoma']
@@ -85,51 +86,51 @@ def triage(data: dict):
     #for now, have follow up value of 0 represent needing human review
     if cecum == 'no':
         outcome = {'follow_up': 0, 'reason': 'Cecum not reached'}
-    if bbps < 6:
+    elif bbps < 6:
         outcome = {'follow_up': 0, 'reason':'Inadequate prep'}
-    if indication == 'sps':
+    elif indication == 'sps':
         outcome = {'follow_up': 0, 'reason': 'Serrated polyposis syndrome'}
     #this seems like something a human should look at
-    if n_adenoma >= 10:
+    elif n_adenoma >= 10:
         outcome = {'follow_up': 1, 'reason': 'Greater than 10 adenomatous polyps'}
 
  
     #3 years
-    if size_ssl >=10:
+    elif size_ssl >=10:
         outcome = {'follow_up': 3, 'reason': 'SSL >= 10mm'}
-    if dysplastic_ssl == 'yes':
+    elif dysplastic_ssl == 'yes':
         outcome = {'follow_up': 3, 'reason': 'SSL with dysplasia'}
-    if size_adenoma >= 10:
+    elif size_adenoma >= 10:
         outcome = {'follow_up': 3, 'reason': 'Adenoma >= 10mm'}
-    if tva == 'yes':
+    elif tva == 'yes':
         outcome = {'follow_up': 3, 'reason': 'Tubulovillous or villous adenoma'}
-    if hgd_adenoma == 'yes':
+    elif hgd_adenoma == 'yes':
         outcome = {'follow_up': 3, 'reason': 'Adenoma with HGD'}
-    if n_adenoma == 0 and n_ssl >= 5 and size_ssl < 10:
+    elif n_adenoma == 0 and n_ssl >= 5 and size_ssl < 10:
         outcome = {'follow_up': 3, 'reason': '5 or more SSL all less than 10mm, no other polyps, no high risk features'}
-    if n_ssl == 0 and 5 <= n_adenoma <= 9 and size_adenoma < 10 and hgd_adenoma == 'no':
+    elif n_ssl == 0 and 5 <= n_adenoma <= 9 and size_adenoma < 10 and hgd_adenoma == 'no':
         outcome = {'follow_up': 3, 'reason': '5-9 adenomas with no high risk features and no SSL'}
-    if n_ssl > 0 and n_adenoma > 0 and 5 <= total_polyps <= 9:
+    elif n_ssl > 0 and n_adenoma > 0 and 5 <= total_polyps <= 9:
         outcome = {'follow_up': 3, 'reason': '5-9 combined adenomas and SSL'}
-    if hp_large == 'yes':
+    elif hp_large == 'yes':
         outcome = {'follow_up': 3, 'reason': 'Hyperplastic polyp >= 10mm'}
     
 
 
     #5 years
-    if n_ssl == 0 and 3 <= n_adenoma <= 4 and size_adenoma < 10 and hgd_adenoma == 'no':
+    elif n_ssl == 0 and 3 <= n_adenoma <= 4 and size_adenoma < 10 and hgd_adenoma == 'no':
         outcome = {'follow_up': 5, 'reason': '3-4 adenomas, no SSL, no high risk features'}
     
-    if 1 <= n_ssl <= 4 and size_ssl < 10 and n_adenoma == 0:
+    elif 1 <= n_ssl <= 4 and size_ssl < 10 and n_adenoma == 0:
         outcome = {'follow_up': 5, 'reason': '1-4 SSL < 10mm no dysplasia no other polyps'}
     
-    if n_ssl > 0 and total_polyps <= 4 and size_ssl < 10 and size_adenoma < 10:
+    elif n_ssl > 0 and total_polyps <= 4 and size_ssl < 10 and size_adenoma < 10:
         outcome = {'follow_up': 5, 'reason': 'Adenoma and SSL present, less than 5 total polyps, no high risk features'}
     
 
     #10 years
 
-    if n_ssl == 0 and n_adenoma < 3 and size_adenoma < 10 and hgd_adenoma == 'no':
+    elif n_ssl == 0 and n_adenoma < 3 and size_adenoma < 10 and hgd_adenoma == 'no':
         outcome = {'follow_up': 10, 'reason': '1-2 adenomas less than 10mm no hgd'}
     
     #if no criteria met, refer for human review
@@ -137,8 +138,8 @@ def triage(data: dict):
         outcome = {'follow_up': 0, 'reason': 'No criteria met, needs human review'}
 
     #see if the patient will age out
-    
-    if outcome['follow_up'] + patient_age > 75:
+    follow_up = outcome['follow_up']
+    if follow_up is not None and follow_up + patient_age > 75:
         outcome = {'follow_up': 0, 'reason': 'Patient aged out'}
         return outcome
     else:
@@ -148,10 +149,12 @@ def triage(data: dict):
 
 
 async def main():
-    with open(DATA_PATH / 'sample_patient_report_2.txt', 'r', encoding = 'utf-8') as f:
+    with open(DATA_PATH / 'sample_patient_report_1.txt', 'r', encoding = 'utf-8') as f:
         report = f.read()
     output = await format_query_json(report)
-    print(output)
+    recommendation = triage(output)
+    print(f'Given this data\n {output}\n\n The recommendation is: {recommendation}')
+    
 
 if __name__ == '__main__':
     asyncio.run(main())
