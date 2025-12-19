ae3503de1405ce7f0d71d770e0eb42808f7897ff
David Lee <doctor.dlee@gmail.com>
2025-12-19 16:44:58 +1300

    add docker compose

--- streamlit_app/ui_app.py
+++ streamlit_app/ui_app.py
@@ -5,22 +5,42 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from pathlib import Path
+import requests
 
 import sys
 
 ROOT = Path(__file__).parent.parent
 sys.path.insert(0, str(ROOT))
 
-print(sys.path)
+#print(sys.path)
 
-
-from app.functions import send_request
 
 load_dotenv()
 
 local_url = 'http://127.0.0.1:8000/triage'
 base_url = os.getenv('AZURE_APP_ENDPOINT')
 azure_url = f'{base_url}/triage'
+
+API_URL = 'http://api:8000/triage' #when running in docker container
+
+
+
+async def send_request(report_text: str, api_url: str):
+    '''
+    Sends a free text report to the API endpoint and returns a recommendation based on the report and database contents
+    '''
+
+    data = {'user_query': report_text}
+    headers = {
+        'x-api-key': os.getenv('MY_API_KEY')
+    }
+
+    response = requests.post(api_url, json = data, headers = headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f'API request failed with status code {response.status_code}: {response.text}')
 
 st.title("Colonoscopy Surveillance Triage Tool")
 st.write("This tool provides surveillance interval recommendations based on the colonoscopy and histology report data you provide.")
@@ -42,7 +62,7 @@ if st.button("Get Recommendation"):
     else:
         with st.spinner('Processing your request...'):
             try:
-                output = asyncio.run(send_request(user_input, azure_url))
+                output = asyncio.run(send_request(user_input, API_URL))
                 recommendation = output['recommendation']
 
             except Exception as e:
