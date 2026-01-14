009d14cb6d8f26a1c15bf591bc1c0db1872430d7
David Lee <doctor.dlee@gmail.com>
2026-01-14 19:25:09 +1300

    update cors

--- app/main.py
+++ app/main.py
@@ -17,11 +17,12 @@ logger = setup_logging()
 
 app = FastAPI(title = 'Colonoscopy triage API')
 
+ORIGINS = os.getenv('CORS_ORIGINS', '').split(',')
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173",
-                     "http://127.0.0.1:5173"],
-    allow_credentials = True,
+    allow_origins = ORIGINS,
+    allow_credentials = False,
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
